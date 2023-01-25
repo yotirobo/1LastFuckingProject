f2e5794381904c91a50e0e5e2f2f146ab89fa7b5
Bar Maor <bmaor2@gmail.com>
2023-01-25 16:21:29 +0200

    hhh

--- server/routes/users.js
+++ server/routes/users.js
@@ -14,44 +14,38 @@ const con = sql.createConnection({
 /* GET users listing. */
 
 router.post("/", function (req, res) {
-  con.connect(err => {
+  let sql = `select username,password from user where username = '${req.body.username}'`
+  con.query(sql, (err, result) => {
     if (err) { console.log(err); return; }
-    let sql = `select username,password from user where username = '${req.body.username}'`
-    con.query(sql, (err, result) => {
-      if (err) { console.log(err); return; }
-      console.log(result[0]);
-      if (result[0]) {
-        if (result[0]?.password === req.body.password) {
-          res.status(200).send(true);
-        }
-        else {
-          res.status(200).send(false)
-        }
+    console.log(result[0]);
+    if (result[0]) {
+      if (result[0]?.password === req.body.password) {
+        res.status(200).send(true);
       }
       else {
         res.status(200).send(false)
       }
-    })
+    }
+    else {
+      res.status(200).send(false)
+    }
   })
 });
 
 router.post("/register", function (req, res) {
-  con.connect(err => {
+  let sql = `select username from user where username = '${req.body.username}'`
+  con.query(sql, (err, result) => {
     if (err) { console.log(err); return; }
-    let sql = `select username from user where username = '${req.body.username}'`
-    con.query(sql, (err, result) => {
-      if (err) { console.log(err); return; }
-      if (result[0]) {
-        res.send(false)
-      }
-      else {
-        sql = `insert into user (username, password, email, adress) values ('${req.body.username}', '${req.body.password}', '${req.body.email}', '${req.body.address}')`;
-        con.query(sql, (err, result) => {
-          if (err) { console.log(err); return; }
-          res.send(true)
-        })
-      }
-    })
+    if (result[0]) {
+      res.send(false)
+    }
+    else {
+      sql = `insert into user (username, password, email, adress) values ('${req.body.username}', '${req.body.password}', '${req.body.email}', '${req.body.address}')`;
+      con.query(sql, (err, result) => {
+        if (err) { console.log(err); return; }
+        res.send(true)
+      })
+    }
   })
 });
 module.exports = router;
