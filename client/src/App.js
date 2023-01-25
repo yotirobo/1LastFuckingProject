90ef9d027d006faf41643767f46033e91b60aacf
Yotam Chanuka <yotamwaks69@gmail.com>
2023-01-25 16:19:24 +0200

    hi2222222222222222

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,31 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import {
-  Route,
-  Link,
-  Routes
-} from "react-router-dom"; import HomePage from './components/HomePage';
+import "./App.css";
+import {Route, Link,Routes} 
+from "react-router-dom"; import HomePage from './components/HomePage';
 import Todo from './components/Todo';
 import ShowInfo from './components/ShowInfo';
 
 function App() {
   return (
-    <>
+    <div className='App'>
       <ul>
-        <li>
-          <Link to="/">Login</Link>
-        </li>
         <li>
           <Link to="/todo">todo</Link>
         </li>
         <li>
-          <Link to="/about">about</Link>
+          {/* <Link to="/about">about</Link> */}
+        </li>
+        <li>
+          <Link to="/">LogOut</Link>
         </li>
       </ul>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/todo" element={<Todo />} />
-        <Route exact path="/about" element={<ShowInfo />} />
+        {/* <Route exact path="/about" element={<ShowInfo />} /> */}
       </Routes>
-    </>
+    </div>
   );
 }
 
