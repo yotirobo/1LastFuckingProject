95a8665e0189a420f34dfc37a0321937db336735
Yotam Chanuka <yotamwaks69@gmail.com>
2023-01-26 17:18:15 +0200

    hi

--- server/routes/users.js
+++ server/routes/users.js
@@ -22,7 +22,11 @@ router.get('/', (req , res )=> {
 })
 
 router.post("/", function (req, res) {
-  let sql = `select username,password from user where username = '${req.body.username}'`
+  let sql = `SELECT username,password
+  FROM user 
+  JOIN Passwords 
+  using(user_id)
+  WHERE username = '${req.body.username}'`
   con.query(sql, (err, result) => {
     if (err) { console.log(err); return; }
     console.log(result[0]);
@@ -45,14 +49,19 @@ router.post("/register", function (req, res) {
   con.query(sql, (err, result) => {
     if (err) { console.log(err); return; }
     if (result[0] || req.body.username === "" || req.body.password === "" || req.body.email === "" || req.body.address === "") {
-      res.send(false)
+      res.status(406).send(false)
     }
     else {
-      sql = `insert into user (username, password, email, adress) values ('${req.body.username}', '${req.body.password}', '${req.body.email}', '${req.body.address}')`;
+      sql = `insert into user (username, email, adress) values ('${req.body.username}', '${req.body.email}', '${req.body.address}')`;
       con.query(sql, (err, result) => {
-        if (err) { console.log(err); return; }
-        res.send(true)
+        if (err) { console.log(err); res.status(406).send(false); }
       })
+      sql = `insert into Passwords (password) values ('${req.body.password}')`;
+      con.query(sql, (err, result) => {
+        if (err) { console.log(err); res.status(406).send(false); }
+        else res.status(200).send(true);
+      })
+      
     }
   })
 });
